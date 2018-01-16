42b0360ac1aa47bbedd7281d60a7f2cab7713dc1
brbelic <br.belic@gmail.com>
2018-01-16 11:07:49 +0100

    Added rouuter for dynamic routing, and tried to implement todo details

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
 import App from './App';
+import TodoDetails from './TodoDetails';
 
 ReactDOM.render(
-	<App />, 
+	<Router>
+        <div>
+            <Route exact path="/" component={App} />
+            <Route path="/:text" component={TodoDetails} />
+        </div>
+    </Router>, 
 	document.getElementById('root')
 );
